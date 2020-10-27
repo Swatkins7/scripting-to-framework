520b34c0fe2ba00efbd802f30b0e32648f63e9ef
Sarah Watkins <s_k_watkins@hotmail.com>
2020-10-27 00:04:25 -0400

    Chapter 7 - Added No-button options & tests
    Added code for the no-button options
    Refactored my existing waits
    Chapter 7 challenge - regex

--- Royale.Tests/CopyDeckTests.cs
+++ Royale.Tests/CopyDeckTests.cs
@@ -1,6 +1,6 @@
+using System.Text.RegularExpressions;
 using Framework.Selenium;
 using NUnit.Framework;
-using OpenQA.Selenium;
 using Royale.Pages;
 
 namespace Tests
@@ -22,14 +22,35 @@ namespace Tests
             Driver.Quit();
         }
 
-        [Test]
+        [Test, Category("copydeck")]
         public void User_can_copy_a_deck()
         {
-            Pages.DeckBuilder.GoTo();
-            Pages.DeckBuilder.AddCardsManually();
+            Pages.DeckBuilder.GoTo().AddCardsManually();
             Pages.DeckBuilder.CopySuggestedDeck();
             Pages.CopyDeck.Yes();
             Assert.That(Pages.CopyDeck.Map.CopiedMessage.Displayed);
+        }
+
+        [Test, Category("copydeck")]
+        public void User_opens_app_store()
+        {
+            Pages.DeckBuilder.GoTo().AddCardsManually();
+            Pages.DeckBuilder.CopySuggestedDeck();
+            Pages.CopyDeck.No().OpenAppStore();
+
+            var title = Regex.Replace(Driver.Title, @"\u200E", string.Empty); //Left-to-right mark
+            title = Regex.Replace(title, @"\u00A0", " "); //non-breaking space
+            Assert.That(title, Is.EqualTo("Clash Royale on the App Store"));
+        }
+
+        [Test, Category("copydeck")]
+        public void User_opens_google_play()
+        {
+            Pages.DeckBuilder.GoTo().AddCardsManually();
+            Pages.DeckBuilder.CopySuggestedDeck();
+            Pages.CopyDeck.No().OpenGooglePlay();
+            //This assertion will fail, because the button redirects to the App Store
+            Assert.AreEqual("Clash Royale - Apps on Google Play", Driver.Title);
         }
     }
 }
